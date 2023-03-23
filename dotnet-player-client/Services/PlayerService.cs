ec0762fb1c9e5570b5db36b941b58794fbdf1379
AarhamH <aarham.haider@gmail.com>
2023-03-22 23:53:35 -0700

    Worked on Home UI
    - designed basic UI template for Home page in Home.xaml

---
+++ dotnet-player-client/Services/PlayerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+
+namespace dotnet_player_client.Services
+{
+    public interface IPlayerService
+    {
+
+    }
+}
--- dotnet-player-client/ViewModels/MainVM.cs
+++ dotnet-player-client/ViewModels/MainVM.cs
@@ -9,5 +9,62 @@ namespace dotnet_player_client.ViewModels
     internal class MainVM : VMBase
     {
         public string BarTitle { get; } = "Audio Player";
+
+        private VMBase? _currentView;
+        public VMBase? CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                if (value == _currentView) return;
+                _currentView?.Dispose();
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private VMBase? _playerView;
+        public VMBase? PlayerView
+        {
+            get { return _playerView; }
+            set
+            {
+                if (value == _playerView) return;
+                _playerView?.Dispose();
+                _playerView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private VMBase? _barView;
+        public VMBase? BarView
+        {
+            get { return _barView; }
+            set
+            {
+                if (value == _barView) return;
+                _barView?.Dispose();
+                _barView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private VMBase? _modalView;
+        public VMBase? ModalView
+        {
+            get { return _modalView; }
+            set
+            {
+                if (value == _modalView) return;
+                _modalView?.Dispose();
+                _modalView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainVM(HomeVM homeView)
+        {
+            // TODO set the views in constructor
+        }
     }
 }
