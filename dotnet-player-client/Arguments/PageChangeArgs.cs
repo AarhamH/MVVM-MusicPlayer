8e20101f50c80227cb9a50436147f4ad264647a7
AarhamH <aarham.haider@gmail.com>
2023-03-23 16:55:31 -0700

    Create PageChangeArgs.cs

---
+++ dotnet-player-client/Arguments/PageChangeArgs.cs
@@ -0,0 +1,13 @@
+﻿using dotnet_player_client.Enumeration;
+using System.Collections.Generic;
+
+
+namespace dotnet_player_client.Arguments
+{
+    public class PageChangeArgs
+    {
+        public PageType Page { get; set; }
+
+        public PageChangeArgs(PageType page) {  this.Page = page; }
+    }
+}
