fdf88496084f5c7cec5968b020536d5c9cc82051
AarhamH <aarham.haider@gmail.com>
2023-03-23 18:32:27 -0700

    Create IFileDrop.cs

---
+++ dotnet-player-client/FileDropInterface/IFileDrop.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace dotnet_player_client.FileDropInterface
+{
+    public interface IFileDrop
+    {
+        Task OnFilesDropAsync(string[] files, object? parameter);
+    }
+}
