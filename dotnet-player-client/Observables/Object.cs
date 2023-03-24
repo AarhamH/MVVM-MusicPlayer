f3a4ea4b7d1cb0f593d1b64bece10bace8d01df2
AarhamH <aarham.haider@gmail.com>
2023-03-23 18:39:26 -0700

    Created Observables

---
+++ dotnet-player-client/Observables/Object.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace dotnet_player_client.Observables
+{
+    public abstract class Object : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ dotnet-player-client/Observables/Wrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace dotnet_player_client.Observables
+{
+    public class Wrapper<O> : Object
+    {
+        private O? _object;
+        public O? Object
+        {
+            get => _object;
+            set
+            {
+                _object = value;
+                OnPropertyChanged();
+            }
+        }   
+    }
+}
