9368d2a67778557c586d539bc51c78f54a368f0c
AarhamH <aarham.haider@gmail.com>
2023-03-25 17:07:42 -0700

    Update DeletePlaylistCommandAsync.cs

--- dotnet-player-client/Command/DeletePlaylistCommandAsync.cs
+++ dotnet-player-client/Command/DeletePlaylistCommandAsync.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using dotnet_player_client.Services;
+using dotnet_player_client.Stores;
+using dotnet_player_client.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace dotnet_player_client.Command
 {
-    internal class DeletPlaylistCommandAsync
+    public class DeletePlaylistCommandAsync: BaseCommandAsync
     {
+        private readonly IPlayerService _musicService;
+        private readonly PLStorage _plStorage;
+        private readonly SongStorage _songStorage;
+        private readonly INavigationService _navigationService;
+        private readonly BrowserNavStorage _browserNavStorage;
+        private readonly ObservableCollection<PLModel>? _observablePL;
+
+        public DeletePlaylistCommandAsync(IPlayerService musicService, PLStorage plStorage, SongStorage songStorage, INavigationService navigationService, BrowserNavStorage browserNavStorage)
+        {
+            _musicService = musicService;
+            _plStorage = plStorage;
+            _songStorage = songStorage;
+            _navigationService = navigationService;
+            _browserNavStorage = browserNavStorage;
+        }
+
+        public DeletePlaylistCommandAsync(IPlayerService musicService, PLStorage plStorage, SongStorage songStorage, INavigationService navigationService, BrowserNavStorage browserNavStorage, ObservableCollection<PLModel> observablePL) : this(musicService, plStorage, songStorage, navigationService, browserNavStorage)
+        {
+            _observablePL = observablePL;
+        }
+
     }
 }
