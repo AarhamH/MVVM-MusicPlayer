91da27cd7d46ba25c4731c7e8247f09c27d099d0
AarhamH <aarham.haider@gmail.com>
2023-04-02 21:24:44 -0700

    Implemented ServiceCollection util

--- dotnet-player-client/App.xaml.cs
+++ dotnet-player-client/App.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using dotnet_player_client.Utilities;
+using dotnet_player_client.ViewModels;
+using dotnet_player_data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +19,35 @@ namespace dotnet_player_client
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider? _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            _serviceProvider = services.AddViewModels()
+                                       .AddNavigation()
+                                       .AddDbContextFactory()
+                                       .AddStores()
+                                       .AddServices()
+                                       .BuildServiceProvider();
+
+            IDbContextFactory<DataContext> dbFactory = _serviceProvider.GetRequiredService<IDbContextFactory<DataContext>>();
+
+            Directory.CreateDirectory("data"); // Create data directory for db files or temp files.
+
+            using (var dbContext = dbFactory.CreateDbContext())
+            {
+                dbContext.Database.Migrate();
+            }
+
+            MainWindow = new MainWindow()
+            {
+                DataContext = _serviceProvider.GetRequiredService<MainVM>()
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        } 
     }
 }
