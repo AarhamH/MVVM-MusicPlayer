8d53af5d6d59bc33f589dc90e142fe7995defcf2
AarhamH <aarham.haider@gmail.com>
2023-05-08 18:56:01 -0700

    Implemented subtle Toolbar UI Change

--- dotnet-player-client/Controls/ToolbarControl.xaml.cs
+++ dotnet-player-client/Controls/ToolbarControl.xaml.cs
@@ -24,5 +24,10 @@ namespace dotnet_player_client.Controls
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
